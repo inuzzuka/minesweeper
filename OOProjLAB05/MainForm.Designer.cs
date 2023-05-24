bee44eb893b2cee9b4e22e6eac8880f5d1d1a465
Andjela <Andjela@INUZUKA>
2023-05-24 08:55:34 +0200

    Add project files.

---
+++ OOProjLAB05/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace OOProjLAB05
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.novaIgraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izmeniMapuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izlazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelMapa = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novaIgraToolStripMenuItem,
+            this.izmeniMapuToolStripMenuItem,
+            this.izlazToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(579, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // novaIgraToolStripMenuItem
+            // 
+            this.novaIgraToolStripMenuItem.Name = "novaIgraToolStripMenuItem";
+            this.novaIgraToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.novaIgraToolStripMenuItem.Text = "Nova igra";
+            // 
+            // izmeniMapuToolStripMenuItem
+            // 
+            this.izmeniMapuToolStripMenuItem.Name = "izmeniMapuToolStripMenuItem";
+            this.izmeniMapuToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.izmeniMapuToolStripMenuItem.Text = "Izmeni mapu";
+            // 
+            // izlazToolStripMenuItem
+            // 
+            this.izlazToolStripMenuItem.Name = "izlazToolStripMenuItem";
+            this.izlazToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.izlazToolStripMenuItem.Text = "Izlaz";
+            this.izlazToolStripMenuItem.Click += new System.EventHandler(this.izlazToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 604);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // panelMapa
+            // 
+            this.panelMapa.Location = new System.Drawing.Point(12, 27);
+            this.panelMapa.Name = "panelMapa";
+            this.panelMapa.Size = new System.Drawing.Size(557, 563);
+            this.panelMapa.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 649);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelMapa);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem novaIgraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izmeniMapuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izlazToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelMapa;
+    }
+}
+
---
+++ OOProjLAB05/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOProjLAB05
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            StampajMapu();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void StampajMapu()
+        {
+            Mapa mapa = new Mapa(9, 9, 10);
+            int velicinaCelije = 30;
+            panelMapa.Controls.Clear();
+
+            for (int i = 0; i < mapa.x; i++)
+            {
+                for (int j = 0; j < mapa.y; j++)
+                {
+                    Control element = new Button();
+                    element.Width = velicinaCelije;
+                    element.Height = velicinaCelije;
+                    element.Left = i * velicinaCelije;
+                    element.Top = j * velicinaCelije;
+
+                    if (mapa.mapa[i, j].JelBomba)
+                        element.BackColor = Color.Red;
+                    else
+                    {
+                        element.BackColor = Color.Gray;
+                        element.Text = mapa.mapa[i, j].brojSusednihBombi.ToString();
+                    }
+
+                    panelMapa.Controls.Add(element);
+                }
+            }
+        }
+        public class Polja
+        {
+            public bool JelBomba { get; set; }
+            public bool jelOtvoreno { get; set; }
+            public int brojSusednihBombi { get; set; }
+
+            public Polja()
+            {
+                JelBomba = false;
+                jelOtvoreno = false;
+                brojSusednihBombi = 0;
+            }
+        }
+        public class Mapa
+        {
+            public Polja[,] mapa;
+            public int x { get; private set; }
+            public int y { get; private set; }
+            private int brojBombi;
+
+            public Mapa(int x, int y, int brojMina)
+            {
+                this.x = Math.Max(x, 9);
+                this.y = Math.Max(y, 9);
+                this.brojBombi = Math.Max(10, (this.x * this.y)/10);
+                mapa = new Polja[this.x, this.y];
+
+                for (int i = 0; i < this.x; i++)
+                    for (int j = 0; j < this.y; j++)
+                        mapa[i, j] = new Polja();
+                PostaviMine();
+                IzracunajSusedneMine();
+
+            }
+            private void PostaviMine()
+            {
+                Random r = new Random();
+                int postavljeneBombe = 0;
+                while(postavljeneBombe < brojBombi)
+                {
+                    int row = r.Next(x);
+                    int column = r.Next(y);
+                    if(!mapa[row, column].JelBomba)
+                    {
+                        mapa[row, column].JelBomba = true;
+                        postavljeneBombe++;
+                    }
+                }
+            }
+            private void IzracunajSusedneMine()
+            {
+                for(int i = 0; i < x; i++)
+                    for(int j = 0; j < y; j++)
+                        if(!mapa[i, j].JelBomba)
+                        {
+                            int susedneBombe = IzbrojSusedneMine(i, j);
+                            mapa[i, j].brojSusednihBombi = susedneBombe;
+                        }
+            }
+            private int IzbrojSusedneMine(int row, int column)
+            {
+                int count = 0;
+                for(int i = -1; i<= 1; i++)
+                    for(int j = -1; j<= 1; j++)
+                    {
+                        int susedanRow = row + i;
+                        int susedanColumn = column + j;
+                        if (susedanRow >= 0 && susedanRow < x && susedanColumn >= 0 && susedanColumn < y)
+                            if (mapa[susedanRow, susedanColumn].JelBomba)
+                                count++;
+                    }
+                return count;
+            }
+        }
+        
+
+        private void izlazToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
